f84171b0da86ecddf4a87544fe60677231f1d203
Jaroslav Šmolík <grissius@gmail.com>
2019-11-11 20:57:23 +0100

    🚀 Add install script

---
+++ scripts/install.sh
@@ -0,0 +1,5 @@
+curl --silent -o .git/hooks/prepare-commit-msg https://raw.githubusercontent.com/grissius/tracker-hook/master/hook.sh
+chmod +x .git/hooks/prepare-commit-msg
+
+echo "Git hook added (.git/hooks/prepare-commit-msg), I hope you like it! ❤️"
+echo "If you face any issues I would love to hear them https://github.com/grissius/tracker-hook"
